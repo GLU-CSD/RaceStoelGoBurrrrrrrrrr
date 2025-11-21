b08de9e716105deb8b9932df355b9e1ad96c6e7e
nathan lammers <nathanlammers01@gmail.com>
2025-11-21 15:11:49 +0100

    test race track

---
+++ Assets/Scripts/Countdown.cs
@@ -0,0 +1,24 @@
+using TMPro;
+using UnityEngine;
+
+public class Countdown : MonoBehaviour
+{
+    [SerializeField] private RaceCarAI[] raceCarAIs;
+    [SerializeField] private CarMovement1 carMovement;
+    [SerializeField] private TextMeshProUGUI countdownText;
+    private float countdown = 4;
+    private void Update()
+    {
+        countdown -= Time.deltaTime;
+        countdownText.text = countdown.ToString();
+        if (countdown <= 0 )
+        {
+            for(int i = 0; i < raceCarAIs.Length; i++)
+            {
+                raceCarAIs[i].enabled = true;
+            }
+            carMovement.enabled = true;
+            Destroy(gameObject);
+        }
+    }
+}
