b7b43bb80a9f0c7ffbaec589e0d71361a32854a7
nathan lammers <nathanlammers01@gmail.com>
2025-11-26 15:42:33 +0100

    placement system yay

---
+++ Assets/Scripts/Placement/RaceManager.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+
+public class RaceManager : MonoBehaviour
+{
+    public CarProgress player;
+    public CarProgress[] aiCars;
+
+    public int playerPosition;
+
+    private void Update()
+    {
+        List<CarProgress> allCars = new List<CarProgress>();
+        allCars.Add(player);
+        allCars.AddRange(aiCars);
+
+        allCars = allCars.OrderByDescending(c => c.totalProgress).ToList();
+
+        playerPosition = allCars.IndexOf(player) + 1;
+    }
+}
