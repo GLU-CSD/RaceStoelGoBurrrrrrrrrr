d4322f0f3d792ad639400646259820415be89b33
nathan lammers <nathanlammers01@gmail.com>
2025-12-01 11:34:29 +0100

    help

--- Assets/Scripts/Countdown.cs
+++ Assets/Scripts/Countdown.cs
@@ -7,17 +7,26 @@ public class Countdown : MonoBehaviour
     [SerializeField] private RealisticCarMovementG29 carMovement;
     [SerializeField] private TextMeshProUGUI countdownText;
     [SerializeField] private GameObject countdownTextObject;
-    private float countdown = 4;
+    [SerializeField] private AudioSource abandonShip;
+    private float countdown = 4.3f;
     private int countdownInt;
     private void Update()
     {
         countdown -= Time.deltaTime;
         countdownInt = (int)countdown;
-        countdownText.text = countdownInt.ToString();
-
-        if (countdown <= 0 )
+        if (countdown <= 3)
         {
-            for(int i = 0; i < raceCarAIs.Length; i++)
+            countdownText.text = countdownInt.ToString();
+        }
+        else
+        {
+            countdownText.text = "3";
+        }
+
+        if (countdown <= 0)
+        {
+            abandonShip.enabled = true;
+            for (int i = 0; i < raceCarAIs.Length; i++)
             {
                 raceCarAIs[i].enabled = true;
             }
